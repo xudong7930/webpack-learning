6d915153c7481b783764b6d91579f1eb28079621
xudong7930 <xudong7930@gmail.com>
2017-09-19 17:28:48 +0800

    last commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,95 @@
 const webpack = require('webpack');
 const path = require('path');
+const glob = require('glob-all')
 const inPrd = process.env.NODE_ENV === 'production';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 module.exports = {
     entry: {
         app: [
             './src/main.js',
             './src/main.scss'
-        ]
+        ],
+        vender: ['jquery']
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
-            // { test: /\.s[ac]ss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            // 处理js文件
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+
+            // 处理font文件
+            { test: /\.ttf|eot|woff|woff2$/, loader: 'file-loader', options: {name: "fonts/[name].[ext]"}},
+
+            // 处理.scss|.sass|css文件
             {
-                test: /\.s[ac]ss$/,
+                test: /\.sass|scss|css$/,
                 use: ExtractTextPlugin.extract({
                     use: ['css-loader', 'sass-loader'],
                     fallback: 'style-loader'
                 })
             },
-            { test: /\.css$/, use: ['style-loader', 'css-loader']},
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+
+            // 处理图片
+            {
+                test: /\.png|jpeg|jpg|gif|svg$/, 
+                use: [
+                    {
+                        loader: 'file-loader',  // 对图片进行复制
+                        options: {name: "img/[name].[ext]"}
+                    },
+                    'img-loader' //对图像进行优化
+                ]
+            }
         ]
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin() //minization
-        new ExtractTextPlugin('[name].[hash].css'),
+        // css文件名称
+        new ExtractTextPlugin('[name].css'),
+
         new webpack.LoaderOptionsPlugin({
             minimize: inPrd
-        })
+        }),
+
+        // 处理css,只将用到的css编译进文件
+        new PurifyCSSPlugin({
+            paths: glob.sync([
+                path.join(__dirname, 'index.html'),
+                path.join(__dirname, 'src/*.html')
+            ]),
+            minimize: inPrd
+        }),
+        
+
+        // 编译前删除的目录和文件
+        new CleanWebpackPlugin(['dist'], {
+            root: __dirname,
+            verbose: inPrd,
+            dry: false,
+            exclude: []
+        }),
+        
+
+        // 将编译后的文件名称保存到json文件中
+        function() {
+            this.plugin('done', (stats) => {
+                require('fs').writeFileSync(
+                    path.join(__dirname, 'dist/manifest.json'), 
+                    JSON.stringify(stats.toJson().assetsByChunkName)
+                )
+            })
+        }
     ]
 }
 
+// product env
 if (inPrd) {
+    // 压缩js文件
     module.exports.plugins.push(
         new webpack.optimize.UglifyJsPlugin()
     )
